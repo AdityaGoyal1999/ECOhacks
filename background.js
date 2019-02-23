edf6bb435c07bcfda3d1e78fbbb3dfed05eccf19
Shivani Aggarwal <shivanisa33@gmail.com>
2019-02-23 15:50:51 -0500

    added desktop notifications.

--- background.js
+++ background.js
@@ -1,3 +1,37 @@
+let data = [{keyword: 'coffee', tip: 'this is a tip'}, {keyword: 'can', tip: 'this is a tip'}];
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	console.log(changeInfo.title);
+	if (changeInfo.title) {
+		let googleSearch = (changeInfo.title).split(" ");
+		let search = "";
+		let tipIndices = [];
+		let size = googleSearch.length;
+		if (googleSearch[size-2] == "Google" && googleSearch[size-1] == "Search") {
+			googleSearch.pop();
+			googleSearch.pop();
+			googleSearch.pop();
+			search = googleSearch.join(" ");
+
+			data.forEach((object, i) => {
+				if (search.includes(object.keyword)) {
+					tipIndices.push(i);
+				}
+			});
+			desktopNotification(tipIndices);
+		}
+	}
 });
+
+function desktopNotification(tipIndices) {
+	var opt = {
+  		type: "basic",
+  		title: "Eco Friendly Tip!",
+  		message: "tip",
+  		iconUrl: "./icon128.png"
+	}
+
+	tipIndices.forEach((index) => {
+		opt.message = data[index].tip;
+		chrome.notifications.create("", opt, () => {});
+	})
+}
