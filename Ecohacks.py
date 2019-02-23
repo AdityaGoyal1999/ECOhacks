6f76f483b2b0c893ed123817243dd02c2b885d42
AdityaGoyal1999 <44474678+AdityaGoyal1999@users.noreply.github.com>
2019-02-23 11:06:02 -0500

    Add files via upload
    The python file for the working of our project.

---
+++ Ecohacks.py
@@ -0,0 +1,65 @@
+""" Challenge 2 -
+
+Code to ask the user about the choices about garbage disposal that he has.
+"""
+from datetime import date
+from datetime import datetime
+from datetime import timedelta
+
+class Activity:
+    """ The every day activities that the user has created for helping out the
+    environment.
+
+    === Attributes ===
+    name: the name of the activity
+    tip: tips regarding the activity that is going on
+    act_date: the date since the user has been maintaining the streak
+    """
+    pass
+
+
+class StreakManager:
+    """ Creates streaks of the activities that the user has generated pr wish to
+    add to his check list system.
+
+    === Attributes ===
+    _streaks: dict for the activity and the number of days that the user has
+        completed. The value is [date, days for the streak.
+    today: the current date.
+    """
+    _streaks: dict
+    today
+
+    def __init__(self) -> None:
+        """ Initializes the class.
+        """
+        self._streaks = {}
+        d = datetime.now()
+        self.today = date(d.year, d.month, d.day)
+
+    def add_activity(self, a: Activity) -> None:
+        """ Adds the activity to the main dictionary"""
+        self._streaks[a] = [self.today, 0]
+
+    def update_today(self) -> None:
+        """ Updates the date of the whole system.
+        """
+        d = datetime.now()
+        self.today = date(d.year, d.month, d.year)
+
+    def calculate_streak(self) -> None:
+        """ Calculates the streak and mutates the activity in the streaks master
+        dictionary.
+        It updates the dates for every day
+
+        * Mutates the self._streaks
+        
+        Precondition: this function is called everyday
+        """
+        for i in self._streaks:
+            new_date = self.today + timedelta(days=-1)
+            # in a continuous streak
+            if i[1] == new_date:
+                i[1] += 1
+            else:
+                i[1] = 0
